f299e696effb5f69cc0d5c7eebcc7bf0190f77f8
Khalid <khalid@deriv.com>
2023-07-30 02:37:29 +0800

    barchart

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ chart.js examples and test
 # Documentation
 
  - [Chart.js official docs](https://www.chartjs.org/docs/latest/)
- - [react-chartjs-2 official docs](https://react-chartjs-2.js.org/) 
+ - [react-chartjs-2 official docs](https://react-chartjs-2.js.org/)
+ - [Examples](https://react-chartjs-2.js.org/examples/)
--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "top",
+    },
+    title: {
+      display: true,
+      text: "Chart.js Bar Chart",
+    },
+  },
+};
+
+const labels = ["January", "February", "March", "April", "May", "June", "July"];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: "Dataset 1",
+      data: [200, 300, 29, 900, 500, 400, 300],
+      backgroundColor: "rgba(255, 99, 132, 0.5)",
+    },
+    {
+      label: "Dataset 2",
+      // random between 0 and 1000
+      data: [50, 100, 150, 200, 250, 300, 350, 400, 450, 500],
+
+      backgroundColor: "rgba(53, 162, 235, 0.5)",
+    },
+  ],
+};
+
+const App = () => {
+  return <Bar options={options} data={data} />;
+};
 
 export default App;
